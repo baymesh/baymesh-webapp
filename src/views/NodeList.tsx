bd05ebf2b096bcc77643561e01b70cfbe714968d
Paul Picazo <ppicazo@gmail.com>
2024-07-06 08:01:43 +0000

    better filters

--- src/views/NodeList.tsx
+++ src/views/NodeList.tsx
@@ -1,4 +1,4 @@
-import { Flex, Form, Grid, TextField, View, Text, ProgressCircle, Heading, Picker, Item } from '@adobe/react-spectrum';
+import { Flex, ProgressCircle, Heading, Picker, Item } from '@adobe/react-spectrum';
 import React, { useEffect } from 'react';
 import { NodeInfo, NodeInfoMap } from '../utils/BayMeshApi';
 import { Link } from 'react-router-dom';
@@ -7,6 +7,9 @@ import {
     flexRender,
     getCoreRowModel,
     useReactTable,
+    getFacetedMinMaxValues,
+    getFacetedRowModel,
+    getFacetedUniqueValues,
     getFilteredRowModel,
     getSortedRowModel,
     ColumnFiltersState,
@@ -34,6 +37,7 @@ const columns = [
     }),
     columnHelper.accessor('hwModel', {
         header: () => <span>Device Model</span>,
+        filterFn: 'equals',
     }),
     columnHelper.accessor('role', {
         header: 'Device Mode',
@@ -41,9 +45,14 @@ const columns = [
     }),
     columnHelper.accessor('hopStart', {
         header: 'Hop Start',
+        sortUndefined: 'last',
+        sortDescFirst: false,
+        filterFn: 'weakEquals',
     }),
     columnHelper.accessor('updatedAt', {
         header: 'Last Updated',
+        sortUndefined: 'last',
+        sortDescFirst: false,
         cell: updatedAt => {
             const timeDiff = Math.floor((curTime - new Date(updatedAt.getValue()).getTime()) / (1000 * 60));
             const hours = Math.floor(timeDiff / 60);
@@ -92,10 +101,14 @@ export default function NodeList({ nodeInfos }: { nodeInfos: NodeInfoMap }) {
             columnFilters,
             sorting,
         },
+        getFacetedRowModel: getFacetedRowModel(), // client-side faceting
+        getFacetedUniqueValues: getFacetedUniqueValues(), // generate unique values for select filter/autocomplete
+        getFacetedMinMaxValues: getFacetedMinMaxValues(), // generate min/max values for range filter
         getCoreRowModel: getCoreRowModel(),
         onColumnFiltersChange: setColumnFilters,
         getFilteredRowModel: getFilteredRowModel(),
         getSortedRowModel: getSortedRowModel(),
+        onSortingChange: setSorting,
     })
 
     if (!nodeInfos || Object.keys(nodeInfos).length === 0 || table.getRowModel().rows.length === 0) {
@@ -105,46 +118,86 @@ export default function NodeList({ nodeInfos }: { nodeInfos: NodeInfoMap }) {
             </Flex>
         )
     } else {
+
+        const columFilter = (columnId: string, label: string) => {
+            const columnFacets = table.getHeaderGroups()[0]?.headers.find((h) => h.id === columnId)?.column.getFacetedUniqueValues();
+            const pickerOptions = columnFacets ? [{ key: 'ALL', label: 'Show All' }].concat(Array.from(columnFacets.entries()).sort((a, b) => {
+                return b[1] - a[1];
+            }).map((value) => {
+                return { key: value[0], label: `${typeof value[0] === 'string' ? humanizeString(value[0]) : value[0]} (${value[1]})` };
+            })) : [{ key: 'ALL', label: 'Show All' }];
+
+            return (<Picker
+                label={`${label} Filter`}
+                defaultSelectedKey={'ALL'}
+                items={pickerOptions}
+                onSelectionChange={(key) => {
+                    if (key === 'ALL') {
+                        setColumnFilters([])
+                    } else {
+                        setColumnFilters(
+                            columnFilters.filter((n) => n.id !== columnId).concat([
+                                {
+                                    id: columnId,
+                                    value: key.toString(),
+                                }
+                            ])
+                        );
+                    }
+                }}
+            >
+                {(item) => (
+                    <Item key={item.key}>{item.label}</Item>
+                )}
+            </Picker>)
+        };
+
         return (
             <Flex direction={"column"} gap="size-100">
-                <Heading level={2}>Nodes seen in last 24 hours</Heading>
-                <Picker
-                    label="Role Filter"
-                    defaultSelectedKey={'ALL'}
-                    items={roleOptions}
-                    onSelectionChange={(key) => {
-                        if (key === 'ALL') {
-                            setColumnFilters([])
-                        } else {
-                            setColumnFilters(
-                                [
-                                    {
-                                        id: 'role',
-                                        value: key.toString(),
-                                    }
-                                ]
-                            );
-                        }
-                    }}
-                >
-                    {(item) => (
-                        <Item key={item.key}>{item.label}</Item>
-                    )}
-                </Picker>
+                <Heading level={2}>Nodes in cache</Heading>
+                <Flex direction={"row"} gap="size-100" marginBottom={10}>
+                    {columFilter('role', 'Role')}
+                    {columFilter('hwModel', 'Device Model')}
+                    {columFilter('hopStart', 'Hop Start')}
+                </Flex>
                 <table>
                     <thead>
                         {table.getHeaderGroups().map(headerGroup => (
                             <tr key={headerGroup.id}>
-                                {headerGroup.headers.map(header => (
-                                    <th key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
+                                {headerGroup.headers.map(header => {
+                                    return (
+                                        <th key={header.id} colSpan={header.colSpan}>
+                                            {header.isPlaceholder ? null : (
+                                                <div
+                                                    className={
+                                                        header.column.getCanSort()
+                                                            ? 'cursor-pointer select-none'
+                                                            : ''
+                                                    }
+                                                    onClick={header.column.getToggleSortingHandler()}
+                                                    title={
+                                                        header.column.getCanSort()
+                                                            ? header.column.getNextSortingOrder() === 'asc'
+                                                                ? 'Sort ascending'
+                                                                : header.column.getNextSortingOrder() === 'desc'
+                                                                    ? 'Sort descending'
+                                                                    : 'Clear sort'
+                                                            : undefined
+                                                    }
+                                                >
+                                                    {flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                                    {{
+                                                        asc: ' 🔼',
+                                                        desc: ' 🔽',
+                                                    }[header.column.getIsSorted() as string] ?? null}
+                                                </div>
                                             )}
-                                    </th>
-                                ))}
+                                        </th>
+                                    )
+                                })}
                             </tr>
                         ))}
                     </thead>
